f3f7b9e81aae8dfe67afc8be902ce38cb2df4a5c
Andrew Vasilchuk <andrew.d.vasilchuk@gmail.com>
2019-05-08 12:54:07 +0300

    added prettier along with husky and due to it configured stylelint

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader" ]
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.scss$/,
